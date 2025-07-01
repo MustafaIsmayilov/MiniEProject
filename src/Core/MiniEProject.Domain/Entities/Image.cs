382b3fdb82d745c46a66a4574431c0d264132f59
MustafaIsmayilov <ismayilov06ms@gmail.com>
2025-07-01 22:01:12 +0400

    Other entities and enum written

---
+++ src/Core/MiniEProject.Domain/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniEProject.Domain.Entities;
+
+public class Image : BaseEntity
+{
+    public string Image_Url { get; set; } = string.Empty!;
+
+    public Guid ProductId { get; set; }
+    public Product? Product { get; set; }
+
+    public bool IsMain { get; set; }
+}
---
+++ src/Core/MiniEProject.Domain/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using MiniEProject.Domain.Enums;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MiniEProject.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = string.Empty!;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; }
+
+    public ProductCondition Condition { get; set; }
+
+    public string? UserId { get; set; }
+    public AppUser User { get; set; } = null!;
+
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
+    public ICollection<Favourite> Favourites { get; set; } = new List<Favourite>();
+    public ICollection<Image> ProductImages { get; set; } = new List<Image>();
+    public ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+}
