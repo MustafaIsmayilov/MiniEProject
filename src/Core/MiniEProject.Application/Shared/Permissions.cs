6d1e428a386bda54c94f47bfa89266a85fc991a4
MustafaIsmayilov <ismayilov06ms@gmail.com>
2025-07-07 11:47:30 +0400

    Last touches(before corrections)

--- src/Core/MiniEProject.Application/Shared/Permissions.cs
+++ src/Core/MiniEProject.Application/Shared/Permissions.cs
@@ -6,7 +6,6 @@ public static class Permissions
     {
         public const string MainCreate = "Category.MainCreate";
         public const string SubCreate = "Category.SubCreate";
-        public const string GetAll = "Category.GetAll";
         public const string Update = "Category.Update";
         public const string Delete = "Category.Delete";
 
@@ -14,32 +13,105 @@ public static class Permissions
         {
             MainCreate,
             SubCreate,
-            GetAll,
             Update,
             Delete
         };
     }
-
     public static class Role
     {
         public const string Create = "Role.Create";
-        public const string GetAllPermission = "Role.GetAllPermission";
+        public const string Update = "Role.Update";
+        public const string Delete = "Role.Delete";
+        public const string GetAllPermissions = "Role.GetAllPermissions";
+        public static List<string> All = new List<string>
+        {
+            GetAllPermissions,
+            Create,
+            Update,
+            Delete
+
+        };
+    }
+    public static class Account
+    {
+        public const string AddRole = "Account.AddRole";
+        public const string Create = "Account.Create";
+
+        public static List<string> All = new()
+        {
+            AddRole,
+            Create
+        };
+    }
+
+    public static class Product
+    {
+        public const string Create = "Product.Create";
+        public const string Update = "Product.Update";
+        public const string Delete = "Product.Delete";
+        public const string GetMy = "Product.GetMy";
+        public const string DeleteProductImage = "Product.DeleteImage";
+        public const string AddProductImage = "Product.AddImage";
+        public const string AddFavourite = "Product.AddFavourite";
+
+        public static List<string> All = new List<string>
+        {
+            Create,
+            Update,
+            Delete,
+            GetMy,
+            DeleteProductImage,
+            AddProductImage,
+            AddFavourite
+        };
+    }
+    public static class Order
+    {
+        public const string Create = "Order.Create";
+        public const string GetMySales = "Order.GetMySales";
+        public const string GetDetail = "Order.GetDetail";
+        public const string Update = "Order.Update";
+        public const string Delete = "Order.Delete";
+        public const string GetMy = "Order.GetMy";
+        public const string GetAll = "Order.GetAll";
+
 
         public static List<string> All = new()
         {
             Create,
-            GetAllPermission
+            Update,
+            GetMySales,
+            GetDetail,
+            Delete,
+            GetMy,
+            GetAll
         };
     }
-
-    public static class Account
+    public static class User
     {
-        public const string AddRole = "Account.AddRole";
+
+        public const string ResetPaswword = "User.ResetPassword";
+        public const string GetAll = "User.GetAll";
+        public const string GetById = "User.GetById";
+        public const string Create = "User.Create";
 
         public static List<string> All = new()
         {
-            AddRole
+            GetAll,
+            GetById,
+            ResetPaswword,
+            Create
         };
     }
+    public static class Review
+    {
+        public const string Create = "Review.Create";
+        public const string Delete = "Review.Delete";
 
+        public static List<string> All = new()
+        {
+            Create,
+            Delete
+        };
+    }
 }
--- src/Infrastructure/MiniEProject.Persistence/Migrations/20250707073159_InitialCreate.cs
+++ src/Infrastructure/MiniEProject.Persistence/Migrations/20250707073159_InitialCreate.cs
@@ -30,11 +30,11 @@ namespace MiniEProject.Persistence.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    FullName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Address = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
                     Age = table.Column<int>(type: "int", nullable: false),
                     BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RefreshToken = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
                     RefreshExpireDate = table.Column<DateTime>(type: "datetime2", nullable: false),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
@@ -205,7 +205,8 @@ namespace MiniEProject.Persistence.Migrations
                         name: "FK_Orders_AspNetUsers_UserId",
                         column: x => x.UserId,
                         principalTable: "AspNetUsers",
-                        principalColumn: "Id");
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
                 });
 
             migrationBuilder.CreateTable(
@@ -214,7 +215,7 @@ namespace MiniEProject.Persistence.Migrations
                 {
                     Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
                     Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
                     Stock = table.Column<int>(type: "int", nullable: false),
                     Condition = table.Column<string>(type: "nvarchar(max)", nullable: false),
@@ -232,7 +233,8 @@ namespace MiniEProject.Persistence.Migrations
                         name: "FK_Products_AspNetUsers_UserId",
                         column: x => x.UserId,
                         principalTable: "AspNetUsers",
-                        principalColumn: "Id");
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
                         name: "FK_Products_Categories_CategoryId",
                         column: x => x.CategoryId,
@@ -260,7 +262,8 @@ namespace MiniEProject.Persistence.Migrations
                         name: "FK_Favourites_AspNetUsers_UserId",
                         column: x => x.UserId,
                         principalTable: "AspNetUsers",
-                        principalColumn: "Id");
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_Favourites_Products_ProductId",
                         column: x => x.ProductId,
@@ -336,7 +339,7 @@ namespace MiniEProject.Persistence.Migrations
                     ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
                     CreatedUser = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
                     UpdatedUser = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
                     UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
                 },
@@ -347,7 +350,8 @@ namespace MiniEProject.Persistence.Migrations
                         name: "FK_Reviews_AspNetUsers_UserId",
                         column: x => x.UserId,
                         principalTable: "AspNetUsers",
-                        principalColumn: "Id");
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_Reviews_Products_ProductId",
                         column: x => x.ProductId,
